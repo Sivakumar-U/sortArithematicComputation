b72f961d238c75cfece9e304b016a200ce73a845
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-04 17:56:14 +0530

    [Siva]Added:To store the results in a dictionary for every computation

--- sortArithematicComputation.sh
+++ sortArithematicComputation.sh
@@ -1,17 +1,30 @@
 #!/bin/bash -x
 
+declare -A resultInDict
+
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
 result=`awk "BEGIN {print $a + ( $b * $c)}"`
 echo "a+b*c="$result
+resultInDict[a+b*c]=$result
 
 result2=`awk "BEGIN {print ($a * $b) + $c}"`
 echo "a*b+c="$result2
+resultInDict[a*b+c]=$result2
 
 result3=`awk "BEGIN {print $c + ($a / $b)}"`
 echo "c+a/b="$result3
+resultInDict[c+a/b]=$result3
 
 result4=`awk "BEGIN {print ($a % $b) + $c}"`
 echo "a%b+c="$result4
+resultInDict[a%b+c]=$result4
+
+echo "Results in dictionary:"
+
+for i in "${!resultInDict[@]}"
+do
+	echo "$i=${resultInDict[$i]}"
+done
